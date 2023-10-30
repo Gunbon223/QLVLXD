7e4a4f90cdff44b8126dec72c491791f999c2aab
minh <minhfli267@gmail.com>
2023-10-30 23:05:51 +0700

    update model

--- CHBHTH/Models/ChiTietDonHang.cs
+++ CHBHTH/Models/ChiTietDonHang.cs
@@ -10,26 +10,32 @@
     public partial class ChiTietDonHang
     {
         [Display(Name = "Chi tiết ")]
-        [Required(ErrorMessage = "Vui lòng nhập tên nhà cung cấp")]
+        [Required(ErrorMessage = "Vui lòng nhập chi tiet")]
         [Key]
         public int CTMaDon { get; set; }
 
         [Display(Name = "Mã đơn hàng")]
+        [Required(ErrorMessage = "Vui lòng nhập mã đơn")]
         public int MaDon { get; set; }
 
         [Display(Name = "Tên sản phẩm")]
+        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
         public int MaSP { get; set; }
 
         [Display(Name = "Số lượng")]
+        [Required(ErrorMessage = "Vui lòng nhập số lượng")]
         public int? SoLuong { get; set; }
 
         [Display(Name = "Đơn giá")]
+        [Required(ErrorMessage = "Vui lòng nhập đơn giá")]
         public decimal? DonGia { get; set; }
 
         [Display(Name = "Thành tiền")]
+        [Required(ErrorMessage = "Vui lòng nhập tên tiền")]
         public decimal? ThanhTien { get; set; }
 
         [Display(Name = "Phương thức thanh toán")]
+        [Required(ErrorMessage = "Vui lòng nhập phương thức thanh toán")]
         public int? PhuongThucThanhToan { get; set; }
 
         public virtual DonHang DonHang { get; set; }
